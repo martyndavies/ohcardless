0f8be5f834110cf41c26806dfab11fef69066267
Martyn <martynrdavies@gmail.com>
2014-09-08 15:11:26 +0100

    change template_id

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ function replyWithCard(email, callback){
   cardEmail.addSubstitution({'-sign_off-': process.env.SIGN_OFF});
   cardEmail.addSubstitution({'-signature-': process.env.SIGNATURE});
   cardEmail.addSubstitution({'-card_image_url-': process.env.CARD_IMAGE_URL});
-  cardEmail.setFilters({"templates":{"settings":{"enabled": 1, "template_id": "43bf6460-bdbf-4770-9623-689f083c06bb"}}});
+  cardEmail.setFilters({"templates":{"settings":{"enabled": 1, "template_id": "325ae5e7-69dd-4b95-b003-b0109f759cfa"}}});
 
   sendgrid.send(cardEmail, function(err, json){
     if (err) {
