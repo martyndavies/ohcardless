f0a378cdeacbdf5c88063319aa5480096fdd13d7
Martyn <martynrdavies@gmail.com>
2014-09-09 12:19:17 +0100

    changing headers

--- app.js
+++ app.js
@@ -28,19 +28,18 @@ function replyWithCard(email, callback){
     from: process.env.FROM_ADDRESS,
     fromname: process.env.FROM_NAME,
     subject: process.env.SUBJECT,
-    html: '<h2>Thanks for requesting a business card!</h2>',
-    text: 'Thanks for asking for a business card, sorry I didn\'t have any on me!'
+    html: '<h2>Thanks for requesting a business card!</h2>', // body tag for text
+    text: 'Thanks for asking for a business card, sorry I didn\'t have any on me!' //body tag for html
   });
 
   // add some extra stuff
+  cardEmail.addHeader('X-SMTPAPI', {"filters": {"templates":{"settings":{"enabled": 1, "template_id": "325ae5e7-69dd-4b95-b003-b0109f759cfa"}}}});
   cardEmail.addSubstitution('-from_name-', process.env.FROM_NAME);
   cardEmail.addSubstitution('-twitter-', process.env.TWITTER);
   cardEmail.addSubstitution('-from_address-', process.env.FROM_ADDRESS);
   cardEmail.addSubstitution('-sign_off-', process.env.SIGN_OFF);
   cardEmail.addSubstitution('-signature-', process.env.SIGNATURE);
   cardEmail.addSubstitution('-card_image_url-', process.env.CARD_IMAGE_URL);
-  cardEmail.addFilter('templates', 'enabled', 1);
-  cardEmail.addFilter('templates', 'template_id', '325ae5e7-69dd-4b95-b003-b0109f759cfa');
   //cardEmail.setFilters({"templates":{"settings":{"enabled": 1, "template_id": "325ae5e7-69dd-4b95-b003-b0109f759cfa"}}});
 
   sendgrid.send(cardEmail, function(err, json){
